d53f34a743e6e8e209b130481bd579c66321e6a5
TangyFish <92803369+TangyFish@users.noreply.github.com>
2021-11-16 08:35:06 -0600

    Add files via upload

--- GuitarStringTester.java
+++ GuitarStringTester.java
@@ -10,6 +10,7 @@ package Guitar_Hero;
 import java.util.*;
 
 public class GuitarStringTester {
+	static GuitarString[] notes;
 
     public static void main(String[] args) 
     {
@@ -17,7 +18,7 @@ public class GuitarStringTester {
         // Create two guitar strings, for concert A and C
         double CONCERT_A0 = 440*Math.pow(1.05956,-24);
         
-        GuitarString[] notes = new GuitarString[37];
+        notes = new GuitarString[37];
         for (int i=0;i<notes.length;i++) {
         	notes[i] = new GuitarString(440*Math.pow(1.05956,i-24));
         }
@@ -30,73 +31,118 @@ public class GuitarStringTester {
  
         // the user types this character
         char key = kb.next().charAt(0);
-
-        // pluck the corresponding string
-        if (key == 'q')  {
-        	 notes[0].pluck();
-        	 for (int i=0;i<44100;i++) {
-             	double sample = notes[0].sample() ;
-             	StdAudio.play(sample);
-             	notes[0].tic();
-             }
+        if (key == 'q') {
+        	plucker(0);
         }
-        if (key == '2')  {
-       	 notes[1].pluck();
-       	 for (int i=0;i<44100;i++) {
-            	double sample = notes[1].sample() ;
-            	StdAudio.play(sample);
-            	notes[1].tic();
-            }
-       }
-        if (key == 'w')  {
-          	 notes[2].pluck();
-          	 for (int i=0;i<44100;i++) {
-               	double sample = notes[2].sample() ;
-               	StdAudio.play(sample);
-               	notes[2].tic();
-               }
-          }
-        if (key == 'e')  {
-          	 notes[3].pluck();
-          	 for (int i=0;i<44100;i++) {
-               	double sample = notes[3].sample() ;
-               	StdAudio.play(sample);
-               	notes[3].tic();
-               }
-          }
-        if (key == '4')  {
-          	 notes[4].pluck();
-          	 for (int i=0;i<44100;i++) {
-               	double sample = notes[4].sample() ;
-               	StdAudio.play(sample);
-               	notes[4].tic();
-               }
-          }
-        if (key == 'r')  {
-          	 notes[5].pluck();
-          	 for (int i=0;i<44100;i++) {
-               	double sample = notes[5].sample() ;
-               	StdAudio.play(sample);
-               	notes[5].tic();
-               }
-          }
-        if (key == '5')  {
-          	 notes[6].pluck();
-          	 for (int i=0;i<44100;i++) {
-               	double sample = notes[6].sample() ;
-               	StdAudio.play(sample);
-               	notes[6].tic();
-               }
-          }
-        
-        
-        
-       
-       
-            
-        
-       
-        
+        if (key == '2') {
+        	plucker(1);
+        }
+        if (key == 'w') {
+        	plucker(2);
+        }
+        if (key == 'e') {
+        	plucker(3);
+        }
+        if (key == '4') {
+        	plucker(4);
+        }
+        if (key == 'r') {
+        	plucker(5);
+        }
+        if (key == '5') {
+        	plucker(6);
+        }
+        if (key == 't') {
+        	plucker(7);
+        }
+        if (key == 'y') {
+        	plucker(8);
+        }
+        if (key == '7') {
+        	plucker(9);
+        }
+        if (key == 'u') {
+        	plucker(10);
+        }
+        if (key == '8') {
+        	plucker(11);
+        }
+        if (key == 'i') {
+        	plucker(12);
+        }
+        if (key == '9') {
+        	plucker(13);
+        }
+        if (key == 'o') {
+        	plucker(14);
+        }
+        if (key == 'p') {
+        	plucker(15);
+        }
+        if (key == '-') {
+        	plucker(16);
+        }
+        if (key == '[') {
+        	plucker(17);
+        }
+        if (key == '=') {
+        	plucker(18);
+        }
+        if (key == 'z') {
+        	plucker(19);
+        }
+        if (key == 'x') {
+        	plucker(20);
+        }
+        if (key == 'd') {
+        	plucker(21);
+        }
+        if (key == 'c') {
+        	plucker(22);
+        }
+        if (key == 'f') {
+        	plucker(23);
+        }
+        if (key == 'v') {
+        	plucker(24);
+        }
+        if (key == 'g') {
+        	plucker(25);
+        }
+        if (key == 'b') {
+        	plucker(26);
+        }
+        if (key == 'n') {
+        	plucker(27);
+        }
+        if (key == 'j') {
+        	plucker(28);
+        }
+        if (key == 'm') {
+        	plucker(29);
+        }
+        if (key == 'k') {
+        	plucker(30);
+        }
+        if (key == ',') {
+        	plucker(31);
+        }
+        if (key == '.') {
+        	plucker(32);
+        }
+        if (key == ';') {
+        	plucker(33);
+        }
+        if (key == '/') {
+        	plucker(34);
+        }
+        if (key == '\'') {
+        	plucker(35);
+        }
+        if (key == ' ') {
+        	plucker(36);
+        }
+        // pluck the corresponding string        
         // compute the superposition of the samples
 
         // send the result to standard audio
@@ -104,4 +150,14 @@ public class GuitarStringTester {
 
         // advance the simulation of each guitar string by one step
     }
+	
+
+		private static void plucker(int i) {
+			notes[i].pluck();
+        	 for (int c=0;c<44100;c++) {
+             	double sample = notes[i].sample() ;
+             	StdAudio.play(sample);
+             	notes[i].tic();
+             }
+		}
 }
